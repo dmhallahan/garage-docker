380e30785ea9cc32143170da8ec079331d2c7248
unknown <David Hallahan@BCSWS027.local.bravecs.com>
2015-10-20 11:45:03 -0400

    init settings from neo

---
+++ start.sh
@@ -0,0 +1,12 @@
+cd /tmp
+
+# try to remove the repo if it already exists
+rm -rf <git repo name>; true
+
+git clone https://secure.bravecs.com/gpresland/15FAT006-Router.git
+
+cd 15FAT006-Router
+
+npm install
+
+node .
